dc20f8e8b67dfbbb0cc4a62c867805d33494b138
momowei <megwu2020@gmail.com>
2023-10-18 22:32:30 +0200

    basic operators

---
+++ script.js
@@ -0,0 +1,51 @@
+//console.log("wulal");
+
+let firstName = "momo";
+console.log(firstName);
+
+lastName="Wu";
+console.log(lastName);
+
+let firstWeek = true;
+//console.log(firstWeek);
+
+console.log(typeof firstWeek);
+
+firstWeek = "happy";
+console.log(typeof firstWeek);
+
+//console.log(typeof 33);
+//console.log(typeof "wulal")
+//console.log(typeof "lulu")
+//console.log(firstWeek);
+
+let x = 3
+x +=10;
+console.log(x);
+
+x--;
+console.log(x);
+
+x++;
+console.log(x);
+
+x += 10;
+console.log(x);
+
+x *= 4;
+console.log(x);
+
+
+const now = 2023
+const myAge = now - 1994;
+const ageBrother = now - 1998;
+console.log(myAge);
+console.log(ageBrother);
+console.log(myAge,ageBrother);
+
+console.log(firstName+lastName);
+console.log(firstName +" "+lastName+""+firstName)
+
+console.log(myAge < ageBrother);
+console.log(myAge >= 24);
+console.log(myAge - 10 <= ageBrother+20);
